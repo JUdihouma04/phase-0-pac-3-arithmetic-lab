07b476de9107086be80c88e0ab6db22b1f60be2d
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-08-04 14:21:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-number = 10;
-
-function add5() {
-  return (number += 5);
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
 }
 
-function divideBy3() {
-  return (number /= 3);
+function mulitiply(a, b) {
+  return a * b;
 }
-
-divideBy3(); //=> 3.3333333333333335
-
-add5(); //=> 8.333333333333334
-
-// reset number
-number = 10;
-
-add5(); //=> 15
-
-divideBy3(); //=> 5
-
-parseInt("2", 10); //=> 2
-
-parseInt("2.2222", 10);
-
-parseInt("nonsense!", 10);
-
-parseFloat("80.123999"); // 80.123999
+function divide(a, b) {
+  return a / b;
+}
+function increment(n) {
+  return ++n;
+}
+function decrement(n) {
+  return --n;
+}
+function makeInt(string) {
+  return parseInt(string, 10);
+}
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
